17d25f4bd6b915b971b5975898a33b6a496e6a9a
theokrueger <theokrueger@airmail.cc>
2024-06-01 16:08:53 -0400

    improve test.sh

--- test.sh
+++ test.sh
@@ -9,6 +9,12 @@ MERGED_PROFILE_FILE="ssgen.profdata"
 
 # ---- #
 
+if [[ "$1" == "help" ]] || [[ "$1" == "" ]]; then
+  echo "Usage: $0 <coverage|profile> [noopen]"
+  echo "Generate coverage report or profile the program"
+  exit
+fi
+
 fail() {
     echo "$@"
     exit 1
@@ -16,17 +22,17 @@ fail() {
 
 # prereqs
 if [[ "$(which grcov; echo $?)" == "1" ]]; then
-    echo "please install grcov using 'cargo install grcov'"
+    echo "Please install grcov on your system"
     exit
 fi
 
 if [[ "$(which llvm-profdata; echo $?)" == "1" ]]; then
-    echo "please install llvm-tools on your system"
+    echo "Please install llvm-tools on your system"
     exit
 fi
 
 if [[ "$(which samply; echo $?)" == "1" ]]; then
-    echo "please install samply on your system"
+    echo "Please install samply on your system"
     exit
 fi
 
@@ -39,20 +45,20 @@ export RUSTDOCFLAGS="-Cpanic=abort"
 export CARGO_INCREMENTAL=0
 export RUST_BACKTRACE=1
 
-# gen coverage
-echo "starting..."
+# begin
+echo "Starting..."
 
 # PWD becomes $OUTPUT DIR
 mkdir -p "$OUTPUT_DIR" && cd "$OUTPUT_DIR" && (rm -rv "$PWD/*" 2> /dev/null ;true) || fail "directory error!"
 
-echo "building executable..."
+echo "Building executable..."
 cargo build || fail "build error!"
 
 if [[ "$1" == "coverage" ]]; then
-  echo "running tests..."
+  echo "Running tests..."
   cargo test -- --test-threads=1 || fail "unit testing error!"
 
-  echo "merging profdata"
+  echo "Merging profdata..."
   llvm-profdata merge --sparse -o "$MERGED_PROFILE_FILE" "$PROFILE_FILE" || fail "profdata error!"
 
   # args shared between show and report
@@ -66,17 +72,17 @@ if [[ "$1" == "coverage" ]]; then
     --object $TEST_BIN \
   "
 
-  echo "showing coverage"
+  echo "Showing coverage..."
   llvm-cov show \
     $LLVM_COV_ARGS \
     --show-instantiations --show-line-counts-or-regions \
     --Xdemangler=rust-demangler \
     || fail "llvm-cov show error!"
 
-  echo "creating report"
+  echo "Creating report..."
   llvm-cov report $LLVM_COV_ARGS || fail "llvm-cov report error!"
 
-  echo "generating html report"
+  echo "Generating HTML report..."
   grcov "$OUTPUT_DIR/$PROFILE_FILE" \
     --binary-path "$TEST_BIN" \
     -s "$PROJECT_ROOT" \
@@ -86,15 +92,17 @@ if [[ "$1" == "coverage" ]]; then
     --llvm-path "$(dirname $(which llvm-profdata))" \
     || fail "grcov error!"
 
-    echo "opening report in browser"
+  if [[ "$2" != "" ]]; then
+    echo "Opening report in browser..."
     xdg-open "$OUTPUT_DIR/html/index.html" || fail "xdg-open error!"
+  fi
 fi # end coverage
 
 if [[ "$1" == "profile" ]]; then
-  echo "profiling full webpage"
+  echo "Profiling full example..."
   FULL_EXAMPLE="$OUTPUT_DIR/example_full/"
   mkdir -p "$FULL_EXAMPLE" || fail "direcory error"
   samply record "$PROJECT_ROOT/target/debug/ssgen" --loglevel Ridiculous --output "$FULL_EXAMPLE" "$PROJECT_ROOT/examples/full/" || fail "samply/ssgen error!"
 fi # end profile
 
-echo "done!"
+echo "Done!"
